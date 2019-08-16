c6fad3dc62a9968b229094036551004dd2129ca6
Naaga <naaga@Naagas-MacBook-Pro.local>
2019-08-15 19:08:27 -0500

    First java project

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -111,6 +112,21 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() < 1) {
+            System.out.println("No jobs found");
+        } else {
+            for (int i = 0; i < someJobs.size(); i++) {
+                for (HashMap<String, String> job : someJobs) {
+                    System.out.println("*****");
+                    for (Map.Entry<String, String> data : job.entrySet()) {
+                        System.out.println(data.getKey() + ": " + data.getValue());
+                    }
+                    System.out.println("*****\n");
+                }
+            }
+        }
     }
 }
+
+
+
